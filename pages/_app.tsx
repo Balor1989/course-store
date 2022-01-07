d015a29c7cf90ea704b62affa965d3d808322012
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-07 12:57:13 +0200

    adds font 'Noto Sans Display'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Course Store</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+      <h1>Hello!</h1>
+    </>
+  );
 }
 
 export default MyApp;
