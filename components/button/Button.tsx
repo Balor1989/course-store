1f632cb32f1a77de43677156d9ebb91a6d629c63
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-07 19:30:24 +0200

    adds HTML prop to button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -4,12 +4,14 @@ import s from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
     
     return (
-        <button className={cn(s.button, {
+        <button className={cn(s.button, className, {
             [s.primary]: appearance === 'primary',
             [s.secondary]: appearance === 'secondary'
-        })}>{children}</button>
+        })}
+        {...props}
+        >{children}</button>
     );
 };
