b4f5612041031a633ea9de4b29d062cd44733f41
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-17 16:57:46 +0200

    Update App.tsx

--- pages/App.tsx
+++ pages/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
             <Htag tag='h1'> Hello!</Htag>
             <Htag tag='h2'> Hello!</Htag>
             <Htag tag='h3'> Hello!</Htag>
-            <Button appearance="primary" arrow='down'>Кнопка</Button>
+            <Button appearance="primary">Кнопка</Button>
             <Button appearance="secondary" arrow="right">Кнопка</Button>
             <Paragraph size="small">fdgdfgdg</Paragraph>
             <Paragraph size="large">fdgdfgdg</Paragraph>
