1743212562bb47fc42c284ca12c7763b0e3358d0
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-19 14:22:24 +0200

    adds grids

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,24 +1,21 @@
-// import cn from 'classnames';
-// import s from './Paragraph.module.css';
+import cn from 'classnames';
+import s from './Layout.module.css';
 import React,{ FunctionComponent } from "react";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { LayoutProps } from "./Layout.props";
-import { Main } from "./Main/Main";
 import { Sidebar } from "./Sidebar/Sidebar";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <Main>
-                <Sidebar />
-                <section >
-                    {children}
-                </section>
-            </Main>
-            <Footer />
-        </>
+        <div className={s.gridWrapper}>
+            <Header className={s.header}/>
+            <Sidebar className={s.sidebar}/>
+            <main className={s.main}>
+                {children}
+            </main>
+            <Footer className={s.footer} />
+        </div>
     );
 };
 
