84f64d12ffeb85604363b3275d7de771834633a6
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-24 15:03:55 +0200

    adds interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,9 +32,9 @@ function Home({ menu}: HomeProps): JSX.Element {
 
 export default renderWithLayout(Home);
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
     const firstCategory = 0;
-    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {firstCategory});
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory });
     return {
         props: {
             firstCategory,
