9de3e91f4a6b9790d3ec84a95be5fa8a3906c4b9
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-29 20:27:55 +0200

    adds  icons array and interface

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,8 +1,20 @@
 import cn from 'classnames';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
+import { firstMenuImem } from '../../interfaces/menu.interface';
 import s from './Menu.module.css';
+import CourseIcon from './menu-icons/courses.svg';
+import BookIcon from './menu-icons/books.svg';
+import ServiceIcon from './menu-icons/services.svg';
+import SupliesIcon from './menu-icons/suplies.svg';
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
+const firstLevelMenu: firstMenuImem[] = [
+    { route: 'courses', name: 'Курсы', icon: <CourseIcon />, id: TopLevelCategory.Courses },
+    { route: 'service', name: 'Сервисы', icon: <ServiceIcon />, id: TopLevelCategory.Courses },
+    { route: 'books', name: 'Книги', icon: <BookIcon />, id: TopLevelCategory.Courses },
+    { route: 'suplies', name: 'Товары', icon: <SupliesIcon />, id: TopLevelCategory.Courses },
+];
 
 export const Menu = (): JSX.Element => {
    const {menu, setMenu, firstCategory} = useContext(AppContext);
