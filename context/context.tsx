39c5d9b8fa5468c75d0eeab5f2cd27388a96bc41
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-29 12:09:24 +0200

    adds context and Menu

---
+++ context/context.tsx
@@ -0,0 +1,22 @@
+import { createContext, PropsWithChildren, useState } from "React";
+import { MenuItem } from "../interfaces/menu.interface";
+import { TopLevelCategory } from "../interfaces/page.interface";
+
+export interface IntContext {
+    menu: MenuItem[];
+    firstCategory: TopLevelCategory;
+    setMenu?: (newMenu: MenuItem[]) => void;
+}
+
+export const AppContext = createContext<IntContext>({ menu: [], firstCategory: TopLevelCategory.Courses });
+
+export const AppContextProvider = ({ menu, firstCategory, children }: PropsWithChildren<IntContext>): JSX.Element => {
+    const [menuState, setMenuState] = useState<MenuItem[]>(menu);
+	const setMenu = (newMenu: MenuItem[]) => {
+		setMenuState(newMenu);
+	};
+
+    return <AppContext.Provider value={{ menu: menuState, firstCategory: firstCategory, setMenu }}>
+        {children}
+        </AppContext.Provider>;
+}; 
