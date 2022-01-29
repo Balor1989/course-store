f898cc243ac063d8bd7a7aab9f6130f060bb3a65
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-01-29 21:13:12 +0200

    adds first:evelMenu build function

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -6,24 +6,50 @@ import s from './Menu.module.css';
 import CourseIcon from './menu-icons/courses.svg';
 import BookIcon from './menu-icons/books.svg';
 import ServiceIcon from './menu-icons/services.svg';
-import SupliesIcon from './menu-icons/suplies.svg';
+import ProductIcon from './menu-icons/suplies.svg';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 
 const firstLevelMenu: firstMenuImem[] = [
     { route: 'courses', name: 'Курсы', icon: <CourseIcon />, id: TopLevelCategory.Courses },
-    { route: 'service', name: 'Сервисы', icon: <ServiceIcon />, id: TopLevelCategory.Courses },
-    { route: 'books', name: 'Книги', icon: <BookIcon />, id: TopLevelCategory.Courses },
-    { route: 'suplies', name: 'Товары', icon: <SupliesIcon />, id: TopLevelCategory.Courses },
+    { route: 'service', name: 'Сервисы', icon: <ServiceIcon />, id: TopLevelCategory.Services },
+    { route: 'books', name: 'Книги', icon: <BookIcon />, id: TopLevelCategory.Books },
+    { route: 'suplies', name: 'Товары', icon: <ProductIcon />, id: TopLevelCategory.Products },
 ];
 
 export const Menu = (): JSX.Element => {
    const {menu, setMenu, firstCategory} = useContext(AppContext);
 
+    const buildFirstLevelMenu = () => {
+        return (
+            <>
+                {firstLevelMenu.map(menu => (
+                    <div key={menu.id}>
+                        <a href={`${menu.route}`}>
+                            <div className={cn(s.firstElement, {
+                                [s.firstLevelActive]: menu.id === firstCategory
+                            })}>
+                                {menu.icon}
+                                <span>{menu.name}</span>
+                            </div>
+                        </a>
+                        {menu.id === firstCategory && buildSecondLevelMenu()}
+                    </div>
+                ))}
+            </>
+        );
+    };
+     
+    const buildSecondLevelMenu = () => {
+        return (
+            <div>
+                
+           </div>
+       )
+   }
+
     return (
-        <div>
-             <ul>
-                {menu.map(m => (<li key={m._id.secondCategory}>{ m._id.secondCategory}</li>))}
-            </ul>
+        <div className={s.menu}>
+         {buildFirstLevelMenu()}
       </div>
     );
 };
