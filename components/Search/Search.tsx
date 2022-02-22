b06488be4ed1db1b5b1b7e0de05edc66dd6a5898
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-22 21:50:07 +0200

    save changes

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -4,13 +4,36 @@ import s from './Search.module.css';
 import { Button, Input } from '..';
 import SearchIcon from './Search.svg';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
   const [search, setSearch] = useState<string>('');
+  const router = useRouter();
+
+  const goToSearch = () => {
+    router.push({
+      pathname: '/search',
+      query: { search },
+    });
+
+    router.push(search);
+  };
+
+  const handleKeyDown = (e: { key: string }) => {
+    if (e.key == 'Enter') {
+      goToSearch();
+    }
+  };
+
   return (
     <div className={cn(className, s.search)} {...props}>
-      <Input placeholder="Search..." value={search} onChange={e => setSearch(e.target.value)} />
-      <Button className={s.searchButton} appearance={'primary'}>
+      <Input
+        placeholder="Search..."
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <Button className={s.searchButton} appearance={'primary'} onClick={goToSearch}>
         <SearchIcon className={s.icon} />
       </Button>
     </div>
